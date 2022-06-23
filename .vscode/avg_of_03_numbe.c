a9162b2af9914922dbda1caec1e0d035ce0a96d4
shadab <shadabs141btech2025@kccitm.edu.inm>
2022-06-23 18:57:19 +0530

    second prog.

---
+++ .vscode/avg_of_03_numbe.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+int a,b,c;
+
+a=4,b=6,c=8;
+    float average;
+  
+    average= (a+b+c)/3;
+    printf("%f is the average of %d %d %d", average, a,b,c);
+
+return 0;
+    }
