531f6c2b73d21f4346f9a7f9746b809df808b227
shadab <shadabs141btech2025@kccitm.edu.inm>
2022-06-24 22:43:05 +0530

    new cord

---
+++ calculate_gross_salary.c
@@ -0,0 +1,19 @@
+
+/* to calculate the gorsspay of ram*/
+
+
+#include <stdio.h>
+
+int main(){
+
+float basicsalary, houserent ,grosspay , dailypay ;
+dailypay= 480;
+basicsalary= 1200;
+houserent=240;
+grosspay=basicsalary+houserent+ dailypay;
+
+printf("%f is the gross pay \n ", grosspay);
+
+return 0;
+
+}
