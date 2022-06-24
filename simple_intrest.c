535d37c5f5ded73ba857a9e8906ddaab0e0a78dc
shadab <shadabs141btech2025@kccitm.edu.inm>
2022-06-24 22:06:47 +0530

    siimple  intrest

---
+++ simple_intrest.c
@@ -0,0 +1,15 @@
+/* cord to calculate the simple intrest
+*/
+#include <stdio.h>
+int main(){
+int p,t;
+float r, si;
+p= 1000; 
+t=3;
+r= 8.5;
+si = (p*r*t)/100;
+printf("%f is  the simple intrest \n",si);
+
+return 0;
+
+}
