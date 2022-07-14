e2f1b0603469719d2e3c02b0902f6f033df1438c
shadab <shadabs141btech2025@kccitm.edu.inm>
2022-07-14 22:21:32 +0530

    practicee question

---
+++ pa.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+
+int num =52 ;
+printf("enter num");
+
+//scanf("%d",&num);
+
+if(num%6==0 );
+{
+    printf("num is divisible by 6 and 4");
+}
+
+
+return 0;
+}
